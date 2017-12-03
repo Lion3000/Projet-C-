5a1732a872a0e4c2adef6d330c911cc6bfc1920b
Alex Zarzitski <alex.zarzitski@gmail.com>
2017-12-03 23:06:51 +0100

    ...

---
+++ JeuNombreMystere/JeuNombreMystere/UcAfficherListeScores/UcAfficherListeScores.cs
@@ -0,0 +1,21 @@
+﻿using ClasseMetier;
+
+using System.Windows;
+
+namespace JeuNombreMystere.UcAfficherListeScores
+{
+    class UcAfficherListeScores
+    {
+        public void doIt(MainWindow mainWindow)
+        {
+            
+            ListeJoueur listeJoueursActifs = new ListeJoueur();
+            foreach (Joueur joueur in mainWindow.ListeJoueur)
+                if(joueur.NbPartiesJouees > 0)
+                    listeJoueursActifs.Add(joueur);
+            listeJoueursActifs.Sort();
+            FenetreVisualisationDesScores fenetreVisualisationDesScores = new FenetreVisualisationDesScores(listeJoueursActifs);
+            fenetreVisualisationDesScores.Show();
+        }
+    }
+}
