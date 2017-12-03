f1b1308ee68322f13eb5bafef7b0ac82f5d8878e
Camélia Benhmida <benhmida.camelia@gmail.com>
2017-12-03 12:06:45 +0100

    avancement

---
+++ JeuNombreMystere/JeuNombreMystere/UcGestionJoueur/NouveauJoueur.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JeuNombreMystere.UcGestionJoueur
+{
+    /// <summary>
+    /// Logique d'interaction pour NouveauJoueur.xaml
+    /// </summary>
+    public partial class NouveauJoueur : Window
+    {
+        public NouveauJoueur()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Joueur " + Prenom.Text + Nom.Text + " a été ajouté", "Confirmation", MessageBoxButton.OK, MessageBoxImage.Question);
+        }
+    }
+}
